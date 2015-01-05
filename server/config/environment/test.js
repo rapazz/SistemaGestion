ac41f01f7647ff1de5cf5c030e75cfcc3d4b2a9c
rapazz <moises.bravo@rapazz.cl>
2015-01-05 02:06:14 +0000

    cambio en archivo de configuracion

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -7,6 +7,11 @@ module.exports = {
   mongo: {
     uri: 'mongodb://localhost/workspace-test'
   },
+   google: {
+    GOOGLE_ID: '831491199430-r42u6nmguim4pvad4t4o78t653pvv1eo.apps.googleusercontent.com',
+    GOOGLE_SECRET: 'l9yeymvFoR97DGipcCLxGix2',
+    DOMAIN: 'http://gestionkcc-rapazz.c9.io:80'
+  },
   mysql: {
     dialect:'mysql',
     port: 3306,
