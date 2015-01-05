38e5af8e8d2bb05247b50870eed9fac9faf2cb42
rapazz <moises.bravo@rapazz.cl>
2015-01-05 00:42:56 +0000

    cambios en el servidor

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -6,5 +6,22 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     uri: 'mongodb://localhost/workspace-test'
-  }
-};
+  },
+  mysql: {
+    dialect:'mysql',
+    port: 3306,
+    user: 'root',
+    password:'',
+    database:'gestion'
+  
+},
+mailer: {
+  host:'email-smtp.us-east-1.amazonaws.com',
+  port:2588,
+  user:'AKIAJGAHAGBMT56FERRA',
+  pass:'AvWUG/8d5fopPAJSUbGUmytmGziTjuyCMLURN8aUi8dK',
+  mailDefecto: 'ckuknow@gmail.com'
+  
+  
+}
+}
